7afe0b34726c53f47c41d3ff47e91826450325ce
miikepg <84807297+miikepg@users.noreply.github.com>
2021-06-20 17:05:38 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCFfyvAeTzApBaq1Qt56HMi6l6ZKZr7Muw",
+     authDomain: "proyectofinalweb50-51e19.firebaseapp.com",
+     projectId: "proyectofinalweb50-51e19",
+     storageBucket: "proyectofinalweb50-51e19.appspot.com",
+     messagingSenderId: "355306318044",
+     appId: "1:355306318044:web:719825470db33330572c19",
+     measurementId: "G-QN6PBTDD2Q"
+     });
